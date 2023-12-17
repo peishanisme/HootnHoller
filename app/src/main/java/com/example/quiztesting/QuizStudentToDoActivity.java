ea702c934bef1c63237520e61a9ee9467e3b6404
XiaoXuan <hengxiaoxuan2003@gmail.com>
2023-12-17 20:56:16 +0800

    Quiz Module

---
+++ app/src/main/java/com/example/quiztesting/QuizStudentToDoActivity.java
@@ -0,0 +1,36 @@
+package com.example.quiztesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.quiztesting.databinding.ActivityQuizStudentToDoBinding;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class QuizStudentToDoActivity extends AppCompatActivity {
+
+    ActivityQuizStudentToDoBinding binding;
+    FirebaseDatabase database;
+    String uid;
+    ArrayList<String> keyCtgList, keySetList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityQuizStudentToDoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        uid = intent.getStringExtra("uid");
+        keyCtgList = intent.getStringArrayListExtra("keyCtgList");
+        keySetList = intent.getStringArrayListExtra("keySetList");
+
+        database = FirebaseDatabase.getInstance();
+
+
+    }
+}
