117ddd95eaf2f02e2fee1923f750cb225b678932
yongjing479 <122367568+yongjing479@users.noreply.github.com>
2023-12-26 03:40:29 +0800

    parent and educator frontend done except filter & search and navigation

--- app/src/main/java/com/firstapp/hootnholler/Parent_Home_Fragment.java
+++ app/src/main/java/com/firstapp/hootnholler/Parent_Home_Fragment.java
@@ -3,62 +3,76 @@ package com.firstapp.hootnholler;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Parent_Home_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.firstapp.hootnholler.databinding.FragmentParentHomeBinding;
+
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Parent_Home_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Parent_Home_Fragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Parent_Home_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Parent_Home_Fragment newInstance(String param1, String param2) {
-        Parent_Home_Fragment fragment = new Parent_Home_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private View TaskStatusBtn, QuizScoreBtn, FeedbackBtn;
+    private FragmentParentHomeBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_parent__home_, container, false);
+
+        // Inflate the layout for this fragment using View Binding
+        binding = FragmentParentHomeBinding.inflate(inflater, container, false);
+
+        TaskStatusBtn = binding.TaskStatusBtn;
+        QuizScoreBtn = binding.QuizBtn;
+        FeedbackBtn = binding.FeedbackBtn;
+
+        // Set click listeners for each button
+        TaskStatusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the TaskStatusFragment
+                Navigation.findNavController(v).navigate(R.id.action_parent_Home_Fragment2_to_task_status);
+            }
+        });
+
+        QuizScoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the QuizScoreFragment
+                Navigation.findNavController(v).navigate(R.id.action_parent_Home_Fragment2_to_qui_score);
+            }
+        });
+
+        FeedbackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the FeedbackFragment
+                Navigation.findNavController(v).navigate(R.id.action_parent_Home_Fragment2_to_parent_feedback);
+            }
+        });
+
+
+
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Child name dropdown field
+        List<String> items = Arrays.asList("John Tiew", "Andy Tiew");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), R.layout.list_item, items);
+        binding.dropdownField.setAdapter(adapter);
+
+
+    }
+
+
 }
