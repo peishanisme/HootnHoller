11ca18fb4bea0117a71fa8bd8a0bb754d40c729b
user <22004795@siswa.um.edu.my>
2023-12-26 17:13:33 +0800

    testing

--- app/src/main/java/com/firstapp/hootnholler/Announcement_ArrayAdapter.java
+++ app/src/main/java/com/firstapp/hootnholler/Announcement_ArrayAdapter.java
@@ -2,7 +2,7 @@ package com.firstapp.hootnholler;
 
 public class Announcement_ArrayAdapter {
     String title, dateTime;
-
+//hhhhh
     public Announcement_ArrayAdapter(String title, String dateTime) {
         this.title = title;
         this.dateTime = dateTime;
