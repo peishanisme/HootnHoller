6f59135e21d8fc79b3902e442abd0f6fa31a8c1a
Natalie Loh <nataliemingyi@gmail.com>
2023-12-29 15:25:26 +0800

    create assignment firebase halfway

--- app/src/main/java/com/firstapp/hootnholler/Teacher_CreateAss.java
+++ app/src/main/java/com/firstapp/hootnholler/Teacher_CreateAss.java
@@ -3,6 +3,11 @@ package com.firstapp.hootnholler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Teacher_CreateAss extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class Teacher_CreateAss extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_create_ass);
+
+    }
+
+    public void buttonAssign(View v) {
+        DatabaseReference assDatabase;
+        assDatabase = FirebaseDatabase.getInstance().getReference("Assignment");
+
+        EditText addAssTitle = (EditText) findViewById(R.id.assTitle);
+        EditText addDescription = (EditText) findViewById(R.id.assDescription);
+        EditText addDueDate = (EditText) findViewById(R.id.dueDate);
+
+        String AssTitle = addAssTitle.getText().toString();
+        String AssDescription = addDescription.getText().toString();
+        String AssDueDate = addDueDate.getText().toString();
+
+        // Generate a unique key for your data
+        String uniqueId = assDatabase.push().getKey();
+
+        // Perform a null check before using the key
+        if (uniqueId != null) {
+            DatabaseReference assRef = assDatabase.child(uniqueId);
+            assRef.child("title").setValue(AssTitle);
+            assRef.child("description").setValue(AssDescription);
+            assRef.child("dueDate").setValue(AssDueDate);
+        }
     }
 }
