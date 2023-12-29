cab571f82b637eeb54aaaaad8fa607c4d5c6fb8b
Natalie Loh <nataliemingyi@gmail.com>
2023-12-29 14:12:48 +0800

    create ass and edit ass page done

---
+++ app/src/main/java/com/firstapp/hootnholler/Teacher_CreateAss.java
@@ -0,0 +1,14 @@
+package com.firstapp.hootnholler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Teacher_CreateAss extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_create_ass);
+    }
+}
