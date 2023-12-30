2cab2240508d7bf9414f16eb755ba9ded188e296
Natalie Loh <nataliemingyi@gmail.com>
2023-12-31 01:06:42 +0800

    [teacher_createAss]dateTime picker, conversion to timestamp firebase done

--- app/src/main/java/com/firstapp/hootnholler/Teacher_CreateAss.java
+++ app/src/main/java/com/firstapp/hootnholler/Teacher_CreateAss.java
@@ -2,43 +2,170 @@ package com.firstapp.hootnholler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class Teacher_CreateAss extends AppCompatActivity {
+    EditText dueDate;
+    Calendar myCalendar;
+    DatabaseReference assDatabase;
+
+    TextView showDueDate,showDueTime, addTime;
+    ImageButton buttonCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_create_ass);
 
+        dueDate = (EditText) findViewById(R.id.dueDatePicker);
+        Button btnAssign = (Button)findViewById(R.id.btnAssign);
+        showDueDate = (TextView)findViewById(R.id.showDate);
+        showDueTime = (TextView)findViewById(R.id.showTime);
+        buttonCalendar = (ImageButton)findViewById(R.id.btnCalendar);
+        addTime = (TextView)findViewById(R.id.addTime);
+
+        buttonCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDateDialog();
+            }
+        });
+
+        addTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTimeDialog();
+            }
+        });
+
+
+        /*DatePickerDialog.OnDateSetListener dueDateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        dueDate.setOnClickListener(view -> {
+            new DatePickerDialog(Teacher_CreateAss.this,dueDateListener,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        });*/
+
+
+        btnAssign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                assDatabase = FirebaseDatabase.getInstance().getReference("Assignment");
+
+                EditText addAssTitle = (EditText) findViewById(R.id.assTitle);
+                EditText addDescription = (EditText) findViewById(R.id.assDescription);
+                EditText addDueDate = (EditText) findViewById(R.id.dueDatePicker);
+
+                // Generate a unique key for your data
+                String assId = assDatabase.push().getKey();
+
+                // Perform a null check before using the key
+                if (assId != null) {
+                    DatabaseReference assRef = assDatabase.child(assId);
+
+                    String AssTitle = addAssTitle.getText().toString();
+                    String AssDescription = addDescription.getText().toString();
+                    String dueDateString = showDueDate.getText().toString();
+                    String dueTimeString = showDueTime.getText().toString();
+
+                    // Concatenate date and time strings to create a DateTime string
+                    String dueDateTimeString = dueDateString + " " + dueTimeString;
+
+                    // Convert the DateTime string to a Date object
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault());
+                    Date dueDate;
+                    try {
+                        dueDate = dateFormat.parse(dueDateTimeString);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+
+                    // Convert the Date object to a Timestamp
+                    //Timestamp dueTimeStamp = new Timestamp(dueDate);
+
+                    // Convert the Date object to a Firebase Timestamp
+                    com.google.firebase.Timestamp dueTimeStamp = new com.google.firebase.Timestamp(dueDate);
+
+                    //long dueTimeStamp = getCurrentDateInTimestamp();
+
+                    assRef.child("title").setValue(AssTitle);
+                    assRef.child("description").setValue(AssDescription);
+                    assRef.child("dueDate").setValue(dueTimeStamp);
+                    assRef.child("openDate");
+                    assRef.child("uploadTime");
+                    assRef.child("attachment");
+                }
+            }
+        });
     }
 
-    public void buttonAssign(View v) {
-        DatabaseReference assDatabase;
-        assDatabase = FirebaseDatabase.getInstance().getReference("Assignment");
-
-        EditText addAssTitle = (EditText) findViewById(R.id.assTitle);
-        EditText addDescription = (EditText) findViewById(R.id.assDescription);
-        EditText addDueDate = (EditText) findViewById(R.id.dueDate);
-
-        String AssTitle = addAssTitle.getText().toString();
-        String AssDescription = addDescription.getText().toString();
-        String AssDueDate = addDueDate.getText().toString();
-
-        // Generate a unique key for your data
-        String uniqueId = assDatabase.push().getKey();
-
-        // Perform a null check before using the key
-        if (uniqueId != null) {
-            DatabaseReference assRef = assDatabase.child(uniqueId);
-            assRef.child("title").setValue(AssTitle);
-            assRef.child("description").setValue(AssDescription);
-            assRef.child("dueDate").setValue(AssDueDate);
-        }
+    /*private void updateLabel() {
+        String myFormat = "MM/dd/yy EEEE";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        dueDate.setText((dateFormat.format(myCalendar.getTime())));
     }
+
+    private long getCurrentDateInTimestamp(){
+        return Calendar.getInstance().getTimeInMillis();
+    }
+
+    private String[] longIntoString(long milliseconds){
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");
+        return new String[]{
+            dateFormat.format(milliseconds),timeFormat.format(milliseconds)
+        };
+
+    }*/
+
+    private void openTimeDialog() {
+        TimePickerDialog timeDialog = new TimePickerDialog(this,new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hours, int minutes) {
+                int seconds = 0;
+                showDueTime.setText(String.valueOf(hours)+":"+String.valueOf(minutes)+":"+String.valueOf(seconds));
+            }
+        },15,00,true);
+
+        timeDialog.show();
+    }
+
+    private void openDateDialog() {
+        DatePickerDialog dateDialog = new DatePickerDialog(this,new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                showDueDate.setText(String.valueOf(year)+"/"+String.valueOf(month+1)+"/"+String.valueOf(day));
+            }
+        },2023,0,15);
+
+        dateDialog.show();
+    }
+
+
 }
