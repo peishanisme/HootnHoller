f9e6974167407cc48b6abaa4eb93916b0ad1130a
yongjing479 <122367568+yongjing479@users.noreply.github.com>
2024-01-04 22:34:35 +0800

    some of the monitoring and testing merged to test run quiz score

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,6 @@ dependencies {
     implementation("com.squareup.picasso:picasso:2.8")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.squareup.picasso:picasso:2.8")
 }
